e85e853af1cff20226571bb70fd66fc111dd35e4
ivoronline <ivoronline@gmail.com>
2023-09-05 19:43:56 +0200

    Commit

--- src/main/java/com/ivoronline/spring_boot_db_multitenant_filter/filters/MultiTenantFilter.java
+++ src/main/java/com/ivoronline/spring_boot_db_multitenant_filter/filters/MultiTenantFilter.java
@@ -5,11 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.HandlerMapping;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 @Order(1)
 @Component
@@ -24,6 +26,8 @@ public class MultiTenantFilter extends OncePerRequestFilter {
     @Override
     public void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
         throws IOException, ServletException {
+
+        Map<String, String> pathParameters = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
 
         //SET TENANT
         String schema = request.getParameter("schema");
